0a108d7d3748e379792442671660b7b5a6b1856a
alexzborovskii <alexzborovskii@gmail.com>
2023-09-03 10:11:17 +0300

    +templates: base, index, exam, results, navbar

--- driving_theory/exam/urls.py
+++ driving_theory/exam/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from exam.views import  LanguagesAPIView, FillDB, QuestionsAPIView,ImageAPIView, exam
+from exam.views import  LanguagesAPIView, FillDB, QuestionsAPIView,ImageAPIView,ExamPageView, HomePageView, RresultsView
 
 urlpatterns = [
     path('fill-db/',  FillDB.as_view(), name="fill_db"),
     path('languages/',  LanguagesAPIView.as_view(), name="languages"),
     path('get-exam-questions/<int:lang_id>',  QuestionsAPIView.as_view(), name="get_exam_questions"),
     path('get-img/<int:img_id>',  ImageAPIView.as_view(), name="get_img"),
-    path('exam/', exam, name='exam'),
+    path('exam/', ExamPageView.as_view(), name='exam'),
+    path('index/', HomePageView.as_view(), name='index'),
+    path('results/', RresultsView.as_view(), name='results'),
 ]
