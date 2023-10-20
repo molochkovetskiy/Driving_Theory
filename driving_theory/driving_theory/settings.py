6739183a5a82e655e8a9dd2797d74e50b6bc5427
alexzborovskii <alexzborovskii@gmail.com>
2023-10-20 23:14:09 +0300

    requirements and gunicorn for render added

--- driving_theory/driving_theory/settings.py
+++ driving_theory/driving_theory/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n52#-y&z+q3v0g$5d7f+!g7mie4_&plsg%vf9v45cia=nsw*a*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
